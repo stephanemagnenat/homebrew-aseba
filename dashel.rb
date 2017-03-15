870593865650342f0c298f7ff60bc22632b4f292
Stéphane Magnenat <stephane@magnenat.net>
2017-03-15 15:24:18 +0100

    Update Dashel to version 1.3.0 and SHA256

--- dashel.rb
+++ dashel.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Dashel < Formula
   homepage "https://github.com/aseba-community/dashel"
-  url "https://github.com/aseba-community/dashel/archive/1.0.8.2.tar.gz"
-  sha1 "40db2b76a62c743fbbccc2d6d425cc7479a9dcac"
+  url "https://github.com/aseba-community/dashel/archive/1.3.0.tar.gz"
+  sha256 "28d8c493f023ad26a60ff6ce5566773fae64c468d31302b516db1f9f928982a8"
 
   depends_on "cmake" => :build
 
