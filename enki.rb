303b2f35630438da1881b62024389fef8a547ead
Stéphane Magnenat <stephane@magnenat.net>
2017-03-15 15:54:23 +0100

    Fixed SDL2 detection in Enki

--- enki.rb
+++ enki.rb
@@ -3,7 +3,7 @@ require "formula"
 class Enki < Formula
   homepage "http://home.gna.org/enki"
   url "https://github.com/enki-community/enki/archive/2.0-pre.20170315.tar.gz"
-  sha256 "ad9fc6e6388d1d89c96588b7010992b27da83bc2c61069d730e99f1b7c1b86d5"
+  sha256 "f4d25604e754a80d2341e92d6d0e5e33479a66e6285a978c05252fc1b96c8025"
 
   depends_on "cmake" => :build
   depends_on "qt"
